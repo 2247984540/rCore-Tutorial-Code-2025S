08e22e3f81f677afff4a51f464e169883fdadb07
little-monster <2247984540@qq.com>
2025-05-02 14:55:00 +0800

    周健_ch3_最终提交

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -2,15 +2,26 @@
 
 const FD_STDOUT: usize = 1;
 
+
+
+
 /// write buf of length `len`  to a file with `fd`
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     trace!("kernel: sys_write");
+
     match fd {
         FD_STDOUT => {
             let slice = unsafe { core::slice::from_raw_parts(buf, len) };
-            let str = core::str::from_utf8(slice).unwrap();
-            print!("{}", str);
-            len as isize
+            match core::str::from_utf8(slice){
+                Ok(s) => {
+                    print!("{}", s);
+                    len as isize
+                },
+                Err(_) => {
+                    crate::task::exit_current_and_run_next();
+                    0
+                },
+            }
         }
         _ => {
             panic!("Unsupported fd in sys_write!");
